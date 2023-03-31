458296858275fd229647eedc19ced3ad1a573d4d
Macley Sousa <macleysousa@hotmail.com>
2023-03-31 11:36:07 -0300

    types MongoStore

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,14 @@
-export const MongoStore: typeof import("./src/MongoStore");
+type options = {
+    session?: string,
+    path?: string,
+}
+
+export class MongoStore {
+
+    constructor({ mongoose, connection, mongo }: { mongoose?: any, connection?: any, mongo?: any });
+
+    sessionExists(session: options): Promise<boolean>;
+    save(options: options): Promise<void>;
+    extract(options: options): Promise<any>;
+    delete(options: options): Promise<void>;
+};
