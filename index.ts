2d0e40bea09ef114c83d39c40cfde49d17244578
Macley Sousa <macleysousa@hotmail.com>
2023-03-31 13:25:35 -0300

    typescrit

---
+++ index.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+import { MongoStore } from './src/MongoStore';
+
+export {
+    MongoStore
+};
---
+++ src/MongoStore.ts
@@ -0,0 +1,76 @@
+import * as fs from 'fs';
+import { Mongoose } from 'mongoose';
+
+interface Options {
+    session: string;
+    path: string;
+    bucket?: any;
+}
+
+type Props = {
+    mongoose: Mongoose;
+};
+
+export class MongoStore {
+    private mongoose: Mongoose;
+
+    constructor({ mongoose }: Props) {
+        if (!mongoose) throw new Error('A valid Mongoose instance is required for MongoStore.');
+        this.mongoose = mongoose;
+    }
+
+    async sessionExists(options: Options): Promise<boolean> {
+        const multiDeviceCollection = this.mongoose.connection.db.collection(`whatsapp-${options.session}.files`);
+        const hasExistingSession: number = await multiDeviceCollection.countDocuments();
+        return !!hasExistingSession;
+    }
+
+    async save(options: Options): Promise<void> {
+        const bucket = new this.mongoose.mongo.GridFSBucket(this.mongoose.connection.db, {
+            bucketName: `whatsapp-${options.session}`
+        });
+        await new Promise((resolve, reject) => {
+            fs.createReadStream(`${options.session}.zip`)
+                .pipe(bucket.openUploadStream(`${options.session}.zip`))
+                .on('error', err => reject(err))
+                .on('close', () => resolve);
+        });
+        options.bucket = bucket;
+        await this.deletePrevious(options);
+    }
+
+    async extract(options: Options): Promise<void> {
+        const bucket = new this.mongoose.mongo.GridFSBucket(this.mongoose.connection.db, {
+            bucketName: `whatsapp-${options.session}`
+        });
+        return new Promise((resolve, reject) => {
+            bucket.openDownloadStreamByName(`${options.session}.zip`)
+                .pipe(fs.createWriteStream(options.path))
+                .on('error', err => reject(err))
+                .on('close', () => resolve());
+        });
+    }
+
+    async delete(options: { session: string }) {
+        const bucket = new this.mongoose.mongo.GridFSBucket(this.mongoose.connection.db, {
+            bucketName: `whatsapp-${options.session}`
+        });
+        const documents = await bucket.find({
+            filename: `${options.session}.zip`
+        }).toArray();
+
+        documents.map(async doc => {
+            return bucket.delete(doc._id);
+        });
+    }
+
+    private async deletePrevious(options: Options) {
+        const documents = await options.bucket.find({
+            filename: `${options.session}.zip`
+        }).toArray();
+        if (documents.length > 1) {
+            const oldSession = documents.reduce((a: any, b: any) => a.uploadDate < b.uploadDate ? a : b);
+            return options.bucket.delete(oldSession._id);
+        }
+    }
+}
